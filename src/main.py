3d635fa98ecf6acd604c4efcb7e6597a4a9263d4
Kacper Kafara <kacperkafara@gmail.com>
2023-07-06 23:53:24 +0200

    Add fitness by generation plot

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import cli
 import subprocess as sp
 import os
+import jssp
 
 
 def configure_env():
@@ -23,27 +24,23 @@ configure_env()
 args = cli.parse_cli_args()
 run_solver(args)
 
+data_file = args.output_file
+assert data_file.is_file(), f"Solver did not produce valid data output file under path: {data_file}"
 
+data_df = pl.read_csv(data_file, has_header=False, new_columns=[
+                      "event", "gen", "time", "fitness"]).filter(pl.col('event') != 'diversity').select(pl.exclude('column_5'))
+print(data_df)
 
-# data_file = Path(sys.argv[1])
-# assert data_file.is_file(), "Pointed file does not exist or is not a regular file"
-#
-# problem_name = None
-# if len(sys.argv) > 2:
-#     problem_name = sys.argv[2]
-#
-# data_df = pl.read_csv(data_file, has_header=False, new_columns=[
-#                       "event", "gen", "time", "fitness"]).filter(pl.col('event') != 'diversity').select(pl.exclude('column_5'))
-# print(data_df)
-#
-# fig, plot = plt.subplots(nrows=1, ncols=1)
-# jssp.plot_fitness_improvements(data_df, plot)
-# plot.set(
-#     title="Najlepszy osobnik w populacji w danej generacji" +
-#     f', problem: {problem_name}' if problem_name else "",
-#     xlabel="Generacja",
-#     ylabel="Wartość funkcji fitness"
-# )
-#
-# plt.show()
+problem_name = None
+
+fig, plot = plt.subplots(nrows=1, ncols=1)
+jssp.plot_fitness_improvements(data_df, plot)
+plot.set(
+    title="Najlepszy osobnik w populacji w danej generacji" +
+    f', problem: {problem_name}' if problem_name else "",
+    xlabel="Generacja",
+    ylabel="Wartość funkcji fitness"
+)
+
+plt.show()
 
