90610903fa64077e17ff9e1a564a6fd1758a7478
Kacper Kafara <kacperkafara@gmail.com>
2023-07-06 22:30:13 +0200

    Add basic cli

--- src/main.py
+++ src/main.py
@@ -1,38 +1,42 @@
-def main():
-    import sys
-    import polars as pl
-    import matplotlib.pyplot as plt
-    from pathlib import Path
-    import jssp
+import argparse
+import sys
+import polars as pl
+import matplotlib.pyplot as plt
+from pathlib import Path
+import jssp
+import cli
 
+
+def configure_env():
     pl.Config.set_tbl_rows(400)
     pl.Config.set_tbl_cols(10)
     plt.rcParams['figure.figsize'] = (16, 9)
 
-    assert len(sys.argv) > 1, "Expected path to a data file"
 
-    data_file = Path(sys.argv[1])
-    assert data_file.is_file(), "Pointed file does not exist or is not a regular file"
-
-    problem_name = None
-    if len(sys.argv) > 2:
-        problem_name = sys.argv[2]
-
-    data_df = pl.read_csv(data_file, has_header=False, new_columns=[
-                          "event", "gen", "time", "fitness"]).filter(pl.col('event') != 'diversity').select(pl.exclude('column_5'))
-    print(data_df)
-
-    fig, plot = plt.subplots(nrows=1, ncols=1)
-    jssp.plot_fitness_improvements(data_df, plot)
-    plot.set(
-        title="Najlepszy osobnik w populacji w danej generacji" +
-        f', problem: {problem_name}' if problem_name else "",
-        xlabel="Generacja",
-        ylabel="Wartość funkcji fitness"
-    )
-
-    plt.show()
+configure_env()
+args = cli.parse_cli_args()
 
 
-if __name__ == "__main__":
-    main()
+
+# data_file = Path(sys.argv[1])
+# assert data_file.is_file(), "Pointed file does not exist or is not a regular file"
+#
+# problem_name = None
+# if len(sys.argv) > 2:
+#     problem_name = sys.argv[2]
+#
+# data_df = pl.read_csv(data_file, has_header=False, new_columns=[
+#                       "event", "gen", "time", "fitness"]).filter(pl.col('event') != 'diversity').select(pl.exclude('column_5'))
+# print(data_df)
+#
+# fig, plot = plt.subplots(nrows=1, ncols=1)
+# jssp.plot_fitness_improvements(data_df, plot)
+# plot.set(
+#     title="Najlepszy osobnik w populacji w danej generacji" +
+#     f', problem: {problem_name}' if problem_name else "",
+#     xlabel="Generacja",
+#     ylabel="Wartość funkcji fitness"
+# )
+#
+# plt.show()
+
