992927caf52bf9b15212e9e3bc5e96b50f1b60a8
Kacper Kafara <kacperkafara@gmail.com>
2023-08-25 11:51:34 +0200

    Improve schema definition

--- src/model.py
+++ src/model.py
@@ -19,38 +19,41 @@ class Col:
     GENERATION = 'gen'
     TIME = 'time'
     FITNESS = 'fitness'
+    POP_SIZE = 'popsize'
+    DIVERSITY = 'diversity'
+    EVAL_TIME = 'eval_time'
+    SEL_TIME = 'sel_time',
+    CROSS_TIME = 'cross_time',
+    MUT_TIME = 'mut_time',
+    REPL_TIME = 'repl_time',
+    ITER_TIME = 'iter_time'
 
     ALL_COLLS = [
         EVENT,
         GENERATION,
         TIME,
         FITNESS,
+        POP_SIZE,
+        DIVERSITY,
+        EVAL_TIME,
+        SEL_TIME,
+        CROSS_TIME,
+        MUT_TIME,
+        REPL_TIME,
+        ITER_TIME
     ]
 
 
-COL_EVENT = 'event'
-COL_GENERATION = 'gen'
-COL_TIME = 'time'
-COL_FITNESS = 'fitness'
+class Schema:
+    SCHEMA_FOR_EVENT = {
+        Event.NEW_BEST: [Col.GENERATION, Col.TIME, Col.FITNESS],
+        Event.DIVERSITY: [Col.GENERATION, Col.TIME, Col.POP_SIZE, Col.DIVERSITY],
+        Event.BEST_IN_GEN: [Col.GENERATION, Col.TIME, Col.FITNESS],
+        Event.POP_GEN_TIME: [Col.TIME],
+        Event.ITER_INFO: [Col.GENERATION, Col.EVAL_TIME, Col.SEL_TIME,
+                          Col.CROSS_TIME, Col.MUT_TIME, Col.REPL_TIME, Col.ITER_TIME]
+    }
 
-OUTPUT_LABELS = [
-    COL_EVENT,
-    COL_GENERATION,
-    COL_TIME,
-    COL_FITNESS,
-]
-
-EVENT_NEW_BEST = 'newbest'
-EVENT_BEST_IN_GEN = 'bestingen'
-EVENT_DIVERSITY = 'diversity'
-EVENT_POP_GEN_TIME = 'popgentime'
-EVENT_ITER_INFO = 'iterinfo'
-
-OUTPUT_EVENTS = [
-    EVENT_BEST_IN_GEN,
-    EVENT_NEW_BEST,
-    EVENT_DIVERSITY,
-    EVENT_POP_GEN_TIME,
-    EVENT_ITER_INFO,
-]
+    def for_event(event: str) -> list[str]:
+        return Schema.SCHEMA_FOR_EVENT[event]
 
