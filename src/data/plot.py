2bb1da3a286ba882ae4d588f5403924e34baee40
Kacper Kafara <kacperkafara@gmail.com>
2023-08-26 22:39:03 +0200

    chore: extract common part of plotting functions (#28)

--- src/data/plot.py
+++ src/data/plot.py
@@ -5,23 +5,17 @@ from .model import Col
 
 def plot_best_in_gen(data: pl.DataFrame, plot: plt.Axes):
     """ Expects rows in `data` to comply to schema for `Event.BEST_IN_GEN` """
-    n_series = data.get_column(Col.SID).n_unique()
-    for sid in range(n_series):
-        series_data = (
-            data
-            .lazy()
-            .filter(pl.col(Col.SID) == sid)
-            .sort(Col.GENERATION)
-            .collect()
-        )
-        print(series_data)
-        y_data = series_data.get_column(Col.FITNESS)
-        x_data = series_data.get_column(Col.GENERATION)
-        plt.plot(x_data, y_data, marker='o', linestyle='--', label=f'Series {sid}')
+    plot_column_by_generation(data, plot, Col.FITNESS)
 
 
 def plot_diversity(data: pl.DataFrame, plot: plt.Axes):
     """ Expects rows in `data` to comply to schema for `Event.DIVERSITY` """
+    plot_column_by_generation(data, plot, Col.DIVERSITY)
+
+
+def plot_column_by_generation(data: pl.DataFrame, plot: plt.Axes, column_name: str):
+    """ Expects `data` to be filtered data for single event type and column name to exists in `data.columns`.
+        Also expects `Col.GENERATION` to exist in `data.columns`."""
     series_ids = data.get_column(Col.SID).unique()
     for sid in series_ids:
         series_data = (
@@ -32,6 +26,7 @@ def plot_diversity(data: pl.DataFrame, plot: plt.Axes):
             .collect()
         )
         print(series_data)
-        y_data = series_data.get_column(Col.DIVERSITY)
+        y_data = series_data.get_column(column_name)
         x_data = series_data.get_column(Col.GENERATION)
         plt.plot(x_data, y_data, marker='o', linestyle='--', label=f'Series {sid}')
+
