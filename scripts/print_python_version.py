7a1ae6cf858e1a2a55aa5e044189cb937190578c
Kacper Kafara <kacperkafara@gmail.com>
2023-10-25 16:05:32 +0200

    Add script for adding python version

---
+++ scripts/print_python_version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import sys
+version_major = sys.version_info.major
+version_minor = sys.version_info.minor
+version_micro = sys.version_info.micro
+
+print("Python %d.%d.%d" % (version_major, version_minor, version_micro))
+
