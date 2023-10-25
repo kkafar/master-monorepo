23342186a50a5321156da60b382e96c93d21f251
Kacper Kafara <kacperkafara@gmail.com>
2023-10-25 19:01:10 +0200

    Print os version in test script

--- scripts/print_python_version.py
+++ scripts/print_python_version.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --partition=plgrid
@@ -6,9 +6,11 @@
 #SBATCH --account=plglscclass23-cpu
 
 import sys
+import os
 version_major = sys.version_info.major
 version_minor = sys.version_info.minor
 version_micro = sys.version_info.micro
 
 print("Python %d.%d.%d" % (version_major, version_minor, version_micro))
+print(f"cwd: {os.getcwd()}")
 
