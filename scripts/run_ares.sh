b071227fd69f6132fa5e1225745c61f3488bbbfb
Kacper Kafara <kacperkafara@gmail.com>
2023-10-26 23:13:07 +0200

    Update path to solvers binray

--- scripts/run_ares.sh
+++ scripts/run_ares.sh
@@ -5,5 +5,5 @@ ecrs_dir=${HOME}/master/ecrs
 
 cd ${ecdk_dir}
 
-sbatch ./src/main.py run -i data/instances/ft_instances -m data/metadata/instance_metadata_v2.txt -o output -n 10 -p 10 ../ecrs/target/release/examples/jssp
+sbatch ./src/main.py run -i data/instances/ft_instances -m data/metadata/instance_metadata_v2.txt -o output -n 10 -p 10 ./bin/jssp
 
