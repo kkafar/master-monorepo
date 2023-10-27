492b0c3f8a6593804ff47ffa756d21bbd2c1f024
Kacper Kafara <kacperkafara@gmail.com>
2023-10-27 19:23:00 +0200

    feat: parameterize run_ares script (#74)

---
+++ scripts/playground.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+OPTIND=1
+opt_str="i:"
+
+input_files=""
+
+while getopts "${opt_str}" opt
+do
+  case "${opt}" in
+    i)
+      input_files=${OPTARG}
+      ;;
+  esac
+done
+
+shift $((OPTIND-1))
+
+${input_files:?"Input files must be set"}
+
+
+echo "${input_files}"
