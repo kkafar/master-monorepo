3aab0b4c214a0c5b6ba5b01c5fd9a8d63ba1b571
Kacper Kafara <kacperkafara@gmail.com>
2024-01-26 20:33:04 +0100

    Add LazyModule class

---
+++ ecdk/src/core/lazymodule.py
@@ -0,0 +1,25 @@
+import importlib as ilib
+from typing import Optional
+
+
+class LazyModule:
+    """ Descriptor, aimed to be used inside classes, to load particular modules only when they are used.
+
+        This only looks like instance of module is created for each instance of this class. In reality
+        we take advantage of importlib cache: when the module is loaded by different instance of LazyModule
+        we receive the same reference thanks to the importlib cache.
+    """
+
+
+    def __init__(self, name: str, package: Optional[str] = None) -> None:
+        self._name = name
+        self._package = package
+        self._instance = None
+
+
+    def __get__(self, instance, owner=None):
+        if self._instance is None:
+            self._instance = ilib.import_module(self._name, package=self._package)
+            assert self._instance is not None
+        return self._instance
+
