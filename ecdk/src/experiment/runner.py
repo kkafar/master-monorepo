34b0d9d88e0363d94f050429633acc975abfc6b4
Kacper Kafara <kacperkafara@gmail.com>
2024-01-26 20:49:48 +0100

    fix(ecdk): properly alias hyperqueue module (#131)
    ## Description
    
    <!-- Please describe the motivation & changes introduced by this PR -->
    
    ## Linked issues
    
    <!-- Please use "Resolves #<issue_no> syntax in case this PR should be
    linked to an issue -->
    
    ## Important implementation details
    
    <!-- if any, optional section -->

--- ecdk/src/experiment/runner.py
+++ ecdk/src/experiment/runner.py
@@ -84,13 +84,13 @@ class AresExpScheduler:
 
 class HyperQueueRunner:
     def __init__(self, solver: SolverProxy):
-        import hyperqueue
+        import hyperqueue as hq
         self._solver: SolverProxy = solver
         self._client = hq.Client()  # We try to create client from default options, not passing path to server files rn
 
 
     def run(self, configs: list[ExperimentConfig]) -> None:
-        import hyperqueue
+        import hyperqueue as hq
         # Important thing here is that we only dispatch the jobs, without waiting for their completion, at for least now
         params_iter = solver_params_from_exp_config_collection(configs)
 
