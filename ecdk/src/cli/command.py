3a01f9a723d439795a9105fc5760e25eeb3dc15e
Kacper Kafara <kacperkafara@gmail.com>
2024-03-12 17:29:09 +0100

    Load command implementation only when particular command is run

--- ecdk/src/cli/command.py
+++ ecdk/src/cli/command.py
@@ -1,19 +1,21 @@
 from .args import RunCmdArgs, AnalyzeCmdArgs, PerfcmpCmdArgs
 from context import Context
-from command import run, analyze, perfcmp
 
 
 def handle_cmd_run(ctx: Context, args: RunCmdArgs):
     print(f"RunCommand run with args: {args}, ctx: {ctx}")
+    from command import run
     run(ctx, args)
 
 
 def handle_cmd_analyze(ctx: Context, args: AnalyzeCmdArgs):
     print(f"AnalyzeCommand run with args: {args}")
+    from command import analyze
     analyze(ctx, args)
 
 
 def handle_cmd_perfcmp(ctx: Context, args: PerfcmpCmdArgs):
     print(f"PerfcmpCommand run with args: {args}")
+    from command import perfcmp
     perfcmp(ctx, args)
 
