623a77b2ba0b1f43d523927405df9eb26ca4ddf3
Kacper Kafara <kacperkafara@gmail.com>
2024-03-29 12:37:44 +0100

    Move IndividualTelemetry to separate submodule

---
+++ solver/src/stats/telemetry.rs
@@ -0,0 +1,53 @@
+use std::{rc::Rc, cell::RefCell};
+
+use ecrs::ga::GAMetadata;
+
+use crate::problem::individual::JsspIndividual;
+
+
+#[derive(Debug, Clone, Copy)]
+struct Inner {
+    /// Generation in which this individual was created
+    pub birth_generation: usize,
+
+    // Number of crossovers this individual was part of
+    pub crossover_involvement: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct IndividualTelemetry {
+    state: Rc<RefCell<Inner>>
+}
+
+impl IndividualTelemetry {
+    pub fn new(birth_generation: usize, crossover_involvement: usize) -> Self {
+        Self {
+            state: Rc::new(RefCell::new(Inner {
+                birth_generation,
+                crossover_involvement,
+            })),
+        }
+    }
+
+    pub fn dummy() -> Self {
+        Self::new(0, 0)
+    }
+
+    pub fn on_crossover(&self) {
+        self.state.borrow_mut().crossover_involvement += 1;
+    }
+
+    pub fn on_create(&self, generation: usize) {
+        let mut state = self.state.borrow_mut();
+        state.birth_generation = generation;
+        state.crossover_involvement = 0;
+    }
+
+    pub fn birth_generation(&self) -> usize {
+        self.state.borrow().birth_generation
+    }
+
+    pub fn crossover_involvement(&self) -> usize {
+        self.state.borrow().crossover_involvement
+    }
+}
