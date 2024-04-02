306344f5b632ef8744ece6cfbf5a931abd69736e
Kacper Kafara <kacperkafara@gmail.com>
2024-04-02 12:55:38 +0200

    refact(ecdk): liquidate core.conversion module (#217)
    ## Linked issues <!-- Please use "Resolves #<issue_no> syntax in case
    this PR should be linked to an issue -->
    
    Closes #207

--- ecdk/src/ecdk/experiment/model.py
+++ ecdk/src/ecdk/experiment/model.py
@@ -269,9 +269,16 @@ class Experiment:
 
     @classmethod
     def from_dict(cls, exp_dict: dict) -> 'Experiment':
-        return cls(name=exp_dict["name"],
-                   instance=exp_dict["instance"],
-                   config=exp_dict["config"])
+        # Some hacks here as Python does not use any kin o type annotation
+        # so it does not know how to serialize nested objects... what a language!
+        if exp_dict.get("id") is not None:
+            return InstanceMetadata.from_dict(exp_dict)
+        elif exp_dict.get("input_file") is not None:
+            return ExperimentConfig.from_dict(exp_dict)
+        else:
+            return cls(name=exp_dict["name"],
+                       instance=exp_dict["instance"],
+                       config=exp_dict["config"])
 
 
 @dataclass
